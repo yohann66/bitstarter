79f0a6c45a3d3c2bdda55b7430eb78f8888097be
root <root@ip-172-31-46-236.us-west-2.compute.internal>
2013-07-10 15:33:43 +0000

    chevre2

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(index.html);
+    response.send("hello world 2!");
+    console.log(index.html);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
