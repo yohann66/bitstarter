79d4299008229469b1d0e92bb6adf6f25d7dd721
root <root@ip-172-31-46-236.us-west-2.compute.internal>
2013-07-11 14:12:29 +0000

    test3

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs;
 app.get('/', function(request, response) {
-    response.send(Buffer.toString(fs.readFileSync(index.html)));
+    response.send(Buffer.toString(fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
