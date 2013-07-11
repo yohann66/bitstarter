69d3f09499443ccd058e184d762b34dd3dd47e2a
root <root@ip-172-31-46-236.us-west-2.compute.internal>
2013-07-11 17:10:52 +0000

    test11

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 var fs = require('fs');
-var buffer = require('buffer')
 
 app.get('/home/ubuntu/bitstarter/', function(request, response)
 	{
-	    response.send(Buffer.toString(fs.readFileSync('index.html')));
-	});
+	    var file = fs.readFileSync('index.html', 'utf8');
+	    response.send(file);
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
