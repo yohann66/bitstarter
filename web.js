c08ba38125d6138aa6eb13b2c2496ce2db188347
root <root@ip-172-31-46-236.us-west-2.compute.internal>
2013-07-11 17:11:53 +0000

    test11

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var fs = require('fs');
 
-app.get('/home/ubuntu/bitstarter/', function(request, response)
+app.get('/', function(request, response)
 	{
 	    var file = fs.readFileSync('index.html', 'utf8');
 	    response.send(file);
